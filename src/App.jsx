f81c81fca0e12b0da80029e76e567d43045e1777
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-09-15 21:02:31 +0600

    cart functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Courses from "./components/Courses/Courses";
-import Cart from './components/Cart/Cart';
+import Cart from "./components/Cart/Cart";
 import { useState } from "react";
 
 function App() {
@@ -10,22 +10,45 @@ function App() {
     const [totalHour, setTotalHour] = useState(0);
     const [totalPrice, setTotalPrice] = useState(0);
 
-    const handleAddToCart = course =>{
-        const newSelectedCourses = [...selectedCourses, course];
-        const newRemainingHour = remainingHour - course.credit;
-        setSelectedCourses(newSelectedCourses);
-        setRemainingHour(newRemainingHour);
-    }
+    const handleAddToCart = (course) => {
+        const isObjectInArray = selectedCourses.some(
+            (obj) => obj.course_name === course.course_name
+        );
+        if (!isObjectInArray && remainingHour >= course.credit) {
+            const newSelectedCourses = [...selectedCourses, course];
+            setSelectedCourses(newSelectedCourses);
+
+            const newRemainingHour = remainingHour - course.credit;
+            setRemainingHour(newRemainingHour);
+
+            const newTotalHour = totalHour + course.credit;
+            setTotalHour(newTotalHour);
+
+            const newTotalPrice = totalPrice + course.price;
+            setTotalPrice(newTotalPrice);
+        }
+        
+        if (isObjectInArray){
+            window.alert("This course is already added.");
+        }
+        if (remainingHour < course.credit){
+            window.alert("You've already used up the available 20 hours for course credit.")
+        }
+    };
     return (
         <div className="font-inter p-14 bg-course-background">
             <Header></Header>
             <div className="grid grid-cols-4 my-8 gap-6">
                 <Courses handleAddToCart={handleAddToCart}></Courses>
-                <Cart selectedCourses={selectedCourses} remainingHour={remainingHour}></Cart>
+                <Cart
+                    selectedCourses={selectedCourses}
+                    remainingHour={remainingHour}
+                    totalHour={totalHour}
+                    totalPrice={totalPrice}
+                ></Cart>
             </div>
         </div>
     );
 }
-
 
 export default App;
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,20 +1,30 @@
 import PropTypes from "prop-types";
 
-const Cart = ({ selectedCourses, remainingHour }) => {
+const Cart = ({ selectedCourses, remainingHour, totalHour, totalPrice }) => {
     return (
-        <div className="col-span-1 p-6 bg-white rounded-xl">
-            {/* remaining credit hours */}
-            <h2 className="text-lg font-bold leading-8 text-course-secondary border-b-2 border-course-border pb-4">Credit Hour Remaining {remainingHour} hr</h2>
+        <div className="col-span-1">
+            <div className="h-fit p-6 bg-white rounded-xl">
+                {/* remaining credit hours */}
+                <h2 className="text-lg font-bold leading-8 text-course-secondary border-b-2 border-course-border pb-4">
+                    Credit Hour Remaining {remainingHour} hr
+                </h2>
 
-            {/* selected courses */}
-            <h2 className="text-xl font-bold mt-4">Course Name</h2>
-            <div className="my-6">
-            {
-                selectedCourses.map((selectedCourse, idx) => <h2 key={idx} className="leading-8 text-course-dil">{idx+1} {selectedCourse.course_name}</h2>)
-            }
+                {/* selected courses */}
+                <h2 className="text-xl font-bold mt-4">Course Name</h2>
+                <div className="my-6">
+                    {selectedCourses.map((selectedCourse, idx) => (
+                        <h2 key={idx} className="leading-8 text-course-dil">
+                            {idx + 1} {selectedCourse.course_name}
+                        </h2>
+                    ))}
+                </div>
+                <h2 className="font-medium py-4 border-y-2 border-course-border text-course-diluted">
+                    Total Credit Hour : {totalHour}
+                </h2>
+                <h2 className="font-semibold py-4 text-course-diluted">
+                    Total Price : {totalPrice} USD
+                </h2>
             </div>
-            <h2 className="font-medium py-4 border-y-2 border-course-border text-course-diluted">Total Credit Hour : 13</h2>
-            <h2 className="font-semibold py-4 text-course-diluted">Total Price : 48000 USD</h2>
         </div>
     );
 };
