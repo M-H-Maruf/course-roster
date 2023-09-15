e3ce433ea147ef6062ca02fcd938108808efade8
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-09-15 15:41:41 +0600

    header made into a react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Header from './components/Header/Header';
 
 function App() {
 
   return (
     <div className='font-inter p-14'>
-      <h1 className='text-course-primary text-center font-bold text-4xl'>Course Registration</h1>
+      <Header></Header>
       <div>
 
       </div>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-course-primary text-center font-bold text-4xl'>Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
