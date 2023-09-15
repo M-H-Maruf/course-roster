15a21599aa4c7a8416c2d7c22d087c68027ef47d
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-09-15 15:49:23 +0600

    courses component added and courses data load succeeded

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-
-import './App.css'
-import Header from './components/Header/Header';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Courses from "./components/Courses/Courses";
 
 function App() {
-
-  return (
-    <div className='font-inter p-14'>
-      <Header></Header>
-      <div>
-
-      </div>
-    </div>
-  )
+    return (
+        <div className="font-inter p-14">
+            <Header></Header>
+            <div>
+                <Courses></Courses>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
